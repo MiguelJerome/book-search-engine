40663a87c9a9cdd615c0d35cff077fa0d8fe9b24
MiguelJerome <sharkmaycry@gmail.com>
2022-10-09 22:15:19 -0400

    test mongoose on heroku

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
 //const {typeDefs, resolvers} = require('./schemas');
-//const db = require('./config/connection');
+const db = require('./config/connection');
 const routes = require('./routes');
 const {authMiddleware} = require('./utils/auth');
 
@@ -25,12 +25,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-/*
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-*/
+
 
 app.use(routes);
 /*
@@ -39,7 +39,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   server.applyMiddleware({ app });
   */
 
-//db.once('open', () => {
+db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on http://localhost:${PORT}`));
-//});
+});
 //};
