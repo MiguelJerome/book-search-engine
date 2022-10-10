3037dabed43cc3bedca1067967c300665cbd1ca2
MiguelJerome <sharkmaycry@gmail.com>
2022-10-09 22:41:38 -0400

    test mongoose on heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://user:user123@cluster0.vpzppix.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://user:user123@cluster0.vpzppix.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
