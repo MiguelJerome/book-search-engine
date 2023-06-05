0bbe0b1cbc4b9ac8b67907e5223a5ead8938e478
MiguelJerome <sharkmaycry@gmail.com>
2023-06-04 23:19:19 -0400

    fix server.js

--- server/server.js
+++ server/server.js
@@ -27,21 +27,21 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
+/*
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
+*/
 
 app.use(routes);
-
+/*
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-  
+  */
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on http://localhost:${PORT}`));
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
-};
+//};
